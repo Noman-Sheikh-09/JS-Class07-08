c543378e2b77bd51628e86abc226293303ceccf8
noman <nomanshk310@gmail.com>
2021-12-15 00:52:57 +0500

    Array Lesson

---
+++ script.js
@@ -0,0 +1,37 @@
+// var room = [1,34,65,'Ali',45,76,23,76,65,3.4,2,34,34,65,'Ali',45,76,23,76,65,3.4,2,34,34,65,'Ali',45,76,23,76,65,3.4,2,34,];
+// console.log(room[10]);
+// console.log("length of arrya", room.length);
+
+// room[3] = 'hamza';
+// console.log(room);
+
+
+// write a js program to draw table for 2 
+// example output
+// 2 * 1 = 2
+// 2 * 2 = 4
+// ...
+// 2 * 20 = 40
+
+// var userinput = parseInt(prompt("Enter Number"))  //All Table//
+// for (let index = 1; index <=20; index++) {
+
+//     console.log("index", index*userinput);
+// }
+
+
+// var numbers = [1,2,4,5,64,6,3,6,3,3]
+// numbers.push(123,43,24,25,2,2,)
+// numbers.pop();
+// numbers.pop()
+// numbers.pop()
+// numbers.pop()
+// numbers.pop()
+// numbers.unshift(10,12,23)
+// numbers.shift()
+// numbers.shift()
+// numbers.shift()
+// console.log(numbers);
+var numbers = [1,2,4,5,64,6,3,6,3,3]
+numbers.splice(1,3)
+console.log(numbers);
